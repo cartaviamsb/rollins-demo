396d1de8aa9396e82aeed4a9e3a33c6e2dc3e1ce
cartaviamsb <carlos@sunnybyte.com>
2022-04-08 16:32:07 -0600

    Menu adjustments

--- web/src/components/Menu.js
+++ web/src/components/Menu.js
@@ -1,22 +1,180 @@
 import React, {useState} from"react";
 
-const Menu = () => {
+const MenuGroup = (props) => {
+  return (
+    <div class="link-group py-2 w-full md:w-1/3 border-b-2 border-rollins-gray md:border-none" id={`secondary-group-${props.i}`} >
+      { props.item.map((group, i) => {
+          return i == 0 ? <GroupHeader item = {group} /> : <GroupLink item = {group} />
+        })
+      }
+    </div>
+  );
+};
+
+const GroupHeader = (props) => {
+  return (
+    <a href={props.item.link} class="block py-2 md:py-0 text-base md:text-sm font-bold text-rollins-blue" role="menuitem" tabindex="-1" id={`secondary-header-item-${props.i}`}>{props.item.title}</a>
+  );
+};
+
+const GroupLink = (props) => {
+  return (
+    <a href={props.item.link} class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id={`secondary-link-item-${props.i}`}>{props.item.title}</a>
+  );
+};
+
+const Menu = (props) => {
+
+  const menuItems= [
+    [
+      {
+        title: 'Overview',
+        link: '#overview'
+      }
+    ],
+    [
+      {
+        title: 'About IR',
+        link: '#IR'
+      },
+      {
+        title: 'Philosophy and Procedures',
+        link: '#pap'
+      },
+      {
+        title: 'Meet the Staff',
+        link: '#staff'
+      },
+    ],
+    [
+      {
+        title: 'Policies and Procedures',
+        link: '#Policies and Procedures'
+      },
+      {
+        title: 'Policies',
+        link: '#Policies'
+      },
+      {
+        title: 'FERPA',
+        link: '#FERPA'
+      },
+      {
+        title: 'FERPA FAQs',
+        link: '#FERPA FAQs'
+      },
+      {
+          title: 'Code of Ethics',
+        link: '#Code of Ethics'
+      },
+      {
+          title: 'Institutional Review Board',
+        link: '#Institutional Review Board'
+      },
+      {
+          title: 'Assessment Schedule',
+        link: '#Assessment Schedule'
+      },
+    ],
+    [
+      {
+        title: 'Facts and Figures',
+        link: '#Facts and Figures'
+        },
+      {
+        title: 'Accreditation',
+        link: '#Accreditation'
+        },
+      {
+        title: 'Institutional Identifiers',
+        link: '#Institutional Identifiers'
+        },
+      {
+        title: 'Facts & Figures',
+        link: '#Facts & Figures'
+        },
+      {
+        title: 'Fact Book and Fact Brochure',
+        link: '#Fact Book and Fact Brochure'
+        },
+      {
+        title: 'Common Data Set (CDS)',
+        link: '#Common Data Set (CDS)'
+        },
+    ],
+    [
+      {
+        title: 'Public Reports',
+        link: '#Public Reports'
+        },
+      {
+        title: 'Carnegie Classification',
+        link: '#Carnegie Classification'
+        },
+      {
+        title: 'Athletic Information',
+        link: '#Athletic Information'
+        },
+      {
+        title: 'IPEDS',
+        link: '#IPEDS'
+        },
+      {
+        title: 'NSSE/BCSSE',
+        link: '#NSSE/BCSSE'
+        },
+      {
+        title: 'Great Colleges To Work For',
+        link: '#Great Colleges To Work For'
+        },
+    ],
+    [
+      {
+        title: 'Data Request Form',
+        link: '#Data Request Form'
+        },
+    ],
+    [
+      {
+        title: 'Institutional Survey Account',
+        link: '#Institutional Survey Account'
+        },
+    ],
+    [
+      {
+        title: 'Resources',
+        link: '#Resources'
+        },
+      {
+        title: 'Consumer Information',
+        link: '#Consumer Information'
+        },
+      {
+        title: 'Institutional Research Resources',
+        link: '#Institutional Research Resources'
+        },
+      {
+        title: 'Glossary of Terms',
+        link: '#Glossary of Terms'
+        },
+    ]
+  ];
 
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
-  }
+  };
 
   const [scrolled, setScrolled] = useState(false);
   if (scrolled && window.scrollY <= 0) {
     setScrolled(false);
   } else if (!scrolled && window.scrollY > 0) {
     setScrolled(true);
-  }
+  };
   
-  let headerClass = "secondary-menu w-full";
-  headerClass += scrolled ? " fixed z-30 top-0 bg-rollins-blue text-white border-b-4 border-rollins-yellow" : " bg-rollins-gray text-rollins-blue shadow";
+  let headerClass = "secondary-menu w-full z-30";
+  headerClass += scrolled ? " fixed top-0 bg-rollins-blue text-white border-b-4 border-rollins-yellow" : " absolute bg-rollins-gray text-rollins-blue shadow";
 
   return (
 
@@ -55,56 +213,12 @@ const Menu = () => {
         */}
         { menuOpen && 
           <div  class={`dropdown-menu inline-flex md:flex-wrap flex-col content-start p-4 sm:px-6 lg:px-8 w-full bg-white focus:outline-none h-screen overflow-y-auto ${!scrolled ? 'mt-0 border-b-4 border-rollins-yellow ' : 'mt-1'}`} role="menu" aria-orientation="vertical" aria-labelledby="secondary-menu-button" tabindex="-1">
-            <div class="link-group py-2 w-full md:w-1/3 border-b-2 border-rollins-gray md:border-none">
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm font-bold text-rollins-blue" role="menuitem" tabindex="-1" id="secondary-menu-item-0">Overview</a>
-            </div>
-            <div class="link-group py-2 w-full md:w-1/3 border-b-2 border-rollins-gray md:border-none">
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm font-bold text-rollins-blue" role="menuitem" tabindex="-1" id="secondary-menu-item-1">About IR</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-2">Philosophy & Standards</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-3">Meet the Staff</a>
-            </div>
-            <div class="link-group py-2 w-full md:w-1/3 border-b-2 border-rollins-gray md:border-none">
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm font-bold text-rollins-blue" role="menuitem" tabindex="-1" id="secondary-menu-item-4">Policies and Procedures</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-5">Policies</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-6">FERPA</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-7">FERPA FAQ's</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-8">Code of Ethics</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-9">Institutional Review Board</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-10">Assessment Schedule</a>
-            </div>
-            <div class="link-group py-2 w-full md:w-1/3 border-b-2 border-rollins-gray md:border-none">
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm font-bold text-rollins-blue" role="menuitem" tabindex="-1" id="secondary-menu-item-4">Facts and Figures</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-5">Accreditation</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-6">Institutional Identifiers</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-7">Facts & Figures</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-8">Fact Book and Fact Brochure</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-9">Common Data Set (CDS)</a>
-            </div>
-            <div class="link-group py-2 w-full md:w-1/3 border-b-2 border-rollins-gray md:border-none">
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm font-bold text-rollins-blue" role="menuitem" tabindex="-1" id="secondary-menu-item-4">Public Reports</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-5">Carnegie Classification</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-6">Athletic Information</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-7">IPEDS</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-8">NSSE/BCSSE</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-9">Great Colleges To Work For</a>
-            </div>
-            <div class="link-group py-2 w-full md:w-1/3 border-b-2 border-rollins-gray md:border-none">
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm font-bold text-rollins-blue" role="menuitem" tabindex="-1" id="secondary-menu-item-0">Data Request Form</a>
-            </div>
-            <div class="link-group py-2 w-full md:w-1/3 border-b-2 border-rollins-gray md:border-none">
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm font-bold text-rollins-blue" role="menuitem" tabindex="-1" id="secondary-menu-item-0">Institutional Survey Account</a>
-            </div>
-            <div class="link-group py-2 w-full md:w-1/3 border-b-2 border-rollins-gray md:border-none">
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm font-bold text-rollins-blue" role="menuitem" tabindex="-1" id="secondary-menu-item-1">Resources</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-2">Consumer Information</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-3">Institutional Research Resources</a>
-              <a href="#" class="block py-2 md:py-0 text-base md:text-sm text-gray-700" role="menuitem" tabindex="-1" id="secondary-menu-item-3">Glossary of Terms</a>
-            </div>
+            {menuItems.map((item, i) => (
+              <MenuGroup item={item} i={i} />
+            ))}
           </div>
         }
     </div>
-
-    
     );
   };
         

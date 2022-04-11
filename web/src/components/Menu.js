251be7381f47e7e8fb7ff03eaa0c229845506332
cartaviamsb <carlos@sunnybyte.com>
2022-04-11 09:38:37 -0600

    Update Menu.js

--- web/src/components/Menu.js
+++ web/src/components/Menu.js
@@ -5,7 +5,7 @@ const MenuGroup = (props) => {
     linkClasses = 'text-gray-700';
 
   return (
-    <div class="link-group py-2 w-full md:w-1/3 border-b-2 border-rollins-gray md:border-none" key={`secondary-group-${props.i}`} >
+    <div className="link-group py-2 w-full md:w-1/3 border-b-2 border-rollins-gray md:border-none" key={`secondary-group-${props.i}`} >
       { props.item.map((group, i) => {
           return <GroupItem item = {group} classes = {i === 0 ? headerClasses : linkClasses} />
         })
@@ -16,7 +16,7 @@ const MenuGroup = (props) => {
 
 const GroupItem = (props) => {
   return (
-    <a href={props.item.link} class={`block py-2 md:py-0 text-base md:text-sm ${props.classes}`}  role="menuitem" tabIndex="-1" key={`secondary-item-${props.i}`}>{props.item.title}</a>
+    <a href={props.item.link} className={`block py-2 md:py-0 text-base md:text-sm ${props.classes}`}  role="menuitem" tabIndex="-1" key={`secondary-item-${props.i}`}>{props.item.title}</a>
   );
 };
 
@@ -187,23 +187,23 @@ const Menu = () => {
 
   return (
 
-    <div class={headerClass}>
-      <div class="navbar px-2 sm:px-6 lg:px-8">
-        <div class="relative flex items-center justify-between min-h-12 py-3">
-          <div class="flex-1 flex items-center sm:items-stretch sm:justify-start">
-            <div class="flex w-1/2 text-sm md:text-base">
-              <a href="#" class="font-bold">Institutional Research</a>
+    <div className={headerClass}>
+      <div className="navbar px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between min-h-12 py-3">
+          <div className="flex-1 flex items-center sm:items-stretch sm:justify-start">
+            <div className="flex w-1/2 text-sm md:text-base">
+              <a href="#" className="font-bold">Institutional Research</a>
             </div>
           </div>
 
           {/* Dropdown  */}
-          <div class="relative">
+          <div className="relative">
             <div>
-              <button type="button" class={`flex px-2 border-2 focus:outline-none font-bold text-xs ${!scrolled ? 'border-rollins-blue' : 'border-white'}`} id="secondary-menu-button" aria-expanded="false" aria-haspopup="true"
+              <button type="button" className={`flex px-2 border-2 focus:outline-none font-bold text-xs ${!scrolled ? 'border-rollins-blue' : 'border-white'}`} id="secondary-menu-button" aria-expanded="false" aria-haspopup="true"
                 onClick={() => { toggleMenu() }}
               >
-                <span class="sr-only">Open secondary menu</span>
-                <span class="pr-2 hidden md:block">Menu</span><span>{`${!menuOpen ? '▼' : '▲'}`}</span>
+                <span className="sr-only">Open secondary menu</span>
+                <span className="pr-2 hidden md:block">Menu</span><span>{`${!menuOpen ? '▼' : '▲'}`}</span>
               </button>
             </div>
           </div>
@@ -221,7 +221,7 @@ const Menu = () => {
           To: "transform opacity-0 scale-95"
         */}
         { menuOpen && 
-          <div  class={`dropdown-menu inline-flex md:flex-wrap flex-col content-start p-4 sm:px-6 lg:px-8 w-full bg-white focus:outline-none h-screen overflow-y-auto ${!scrolled ? 'mt-0 border-b-4 border-rollins-yellow ' : 'mt-1'}`} role="menu" aria-orientation="vertical" aria-labelledby="secondary-menu-button" tabindex="-1">
+          <div  className={`dropdown-menu inline-flex md:flex-wrap flex-col content-start p-4 sm:px-6 lg:px-8 w-full bg-white focus:outline-none h-screen overflow-y-auto ${!scrolled ? 'mt-0 border-b-4 border-rollins-yellow ' : 'mt-1'}`} role="menu" aria-orientation="vertical" aria-labelledby="secondary-menu-button" tabindex="-1">
             {menuItems.map((item, i) => (
               <MenuGroup item={item} i={i} />
             ))}

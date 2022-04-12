40248ddb122c3c6a4da6441972437dfbd1fe8c8c
cartaviamsb <carlos@sunnybyte.com>
2022-04-12 16:58:29 -0600

    Update secondary-menu.js

--- web/src/components/secondary-menu.js
+++ web/src/components/secondary-menu.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from"react";
+import React, {useState, useEffect} from "react";
 
 const MenuGroup = (props) => {
-  const headerClasses = 'font-bold text-rollins-blue',
+  const headerClasses = 'font-bold text-blue-500',
     linkClasses = 'text-gray-700';
 
   return (
-    <div className="link-group py-2 w-full md:w-1/3 border-b-2 border-rollins-gray md:border-none" >
+    <div className="link-group py-2 w-full md:w-1/3 border-b-2 border-gray-100 md:border-none" >
       { props.item.map((group, i) => {
           return <GroupItem item={group} classes={i === 0 ? headerClasses : linkClasses} key={`secondary-item-${i}`}/>
         })
@@ -44,12 +44,12 @@ const SecondaryMenu = (props) => {
   }
   
   let headerClass = "secondary-menu w-full z-30";
-  headerClass += scrolled ? " fixed top-0 bg-rollins-blue text-white border-b-4 border-rollins-yellow" : " absolute bg-rollins-gray text-rollins-blue shadow";
+  headerClass += scrolled ? " fixed top-0 bg-blue-500 text-white border-b-4 border-yellow-500" : " absolute bg-gray-100 text-blue-500 shadow";
 
   return (
 
     <div className={headerClass}>
-      <div className="navbar px-2 sm:px-6 lg:px-8">
+      <div className="navbar px-4 max-w-6xl mx-auto">
         <div className="relative flex items-center justify-between min-h-12 py-3">
           <div className="flex-1 flex items-center sm:items-stretch sm:justify-start">
             <div className="flex w-1/2 text-sm md:text-base">
@@ -58,7 +58,7 @@ const SecondaryMenu = (props) => {
           </div>
 
           {/* Dropdown button  */}
-          <button type="button" className={`flex px-2 border-2 focus:outline-none font-bold text-xs ${!scrolled ? 'border-rollins-blue' : 'border-white'}`} id="secondary-menu-button" aria-expanded="false" aria-haspopup="true"
+          <button type="button" className={`flex px-2 border-2 focus:outline-none font-bold text-xs ${!scrolled ? 'border-blue-500' : 'border-white'}`} id="secondary-menu-button" aria-expanded="false" aria-haspopup="true"
             onClick={() => { toggleMenu() }}
           >
             <span className="sr-only">Open secondary menu</span>
@@ -78,10 +78,14 @@ const SecondaryMenu = (props) => {
           To: "transform opacity-0 scale-95"
         */}
         { menuOpen && 
-          <div className={`dropdown-menu inline-flex md:flex-wrap flex-col content-start p-4 sm:px-6 lg:px-8 w-full bg-white focus:outline-none h-screen overflow-y-auto ${!scrolled ? 'mt-0 border-b-4 border-rollins-yellow ' : 'mt-1'}`} role="menu" aria-orientation="vertical" aria-labelledby="secondary-menu-button">
-            {props.menuItems && props.menuItems.map((item, i) => (
-              <MenuGroup item={item} i={i} key={i}/>
-            ))}
+          <div class={`dropdown-container bg-white overflow-y-hidden ${!scrolled ? 'border-b-4 border-yellow-500 ' : ''}`}>
+            <div className="dropdown-layout max-w-6xl mx-auto overflow-y-hidden">
+              <div className="dropdown-menu inline-flex md:flex-wrap p-4 flex-col content-start w-full focus:outline-none h-screen overflow-y-auto" role="menu" aria-orientation="vertical" aria-labelledby="secondary-menu-button">
+                {props.menuItems && props.menuItems.map((item, i) => (
+                  <MenuGroup item={item} i={i} key={i}/>
+                ))}
+              </div>
+            </div>
           </div>
         }
     </div>
